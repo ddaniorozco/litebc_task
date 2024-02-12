ffb47ee23e173af5c699ff199202064374cbcfc2
ddaniorozco <64428779+ddaniorozco@users.noreply.github.com>
2024-02-12 16:37:57 +0200

    Update report_classes.py

--- tools/report_classes.py
+++ tools/report_classes.py
@@ -24,29 +24,25 @@ def visualize_labels(image_folder, label_folder, class_names, output_file):
         for line in lines:
             line = line.decode('ascii', errors='ignore').strip()  # Decode as ASCII
             if not line:
-                continue  # Skip empty lines
+                continue
 
             class_id, _, _, _, _ = map(float, line.split())
 
-            # Count class occurrences
             class_name = class_names[int(class_id)]
             image_class_counts[class_name] += 1
 
-        # Update total class counts
         for class_name, count in image_class_counts.items():
             class_counts[class_name] += count
 
-    # Write the total counts to the report file
     with open(output_file, 'w') as report_file:
         report_file.write("Total Class Counts:\n")
         for class_name, total_count in class_counts.items():
             report_file.write(f"{class_name}: {total_count}\n")
 
 
-# Example usage
 image_folder = '/Users/daniorozco/Desktop/litebc_task/capillary_detection/images'
 label_folder = '/Users/daniorozco/Desktop/litebc_task/capillary_detection/labels'
-class_names = ['BLURRY', 'SHARP', 'BUBBLE']  # Replace with your class names
+class_names = ['BLURRY', 'SHARP', 'BUBBLE']
 output_file = 'report.txt'
 
 visualize_labels(image_folder, label_folder, class_names, output_file)
