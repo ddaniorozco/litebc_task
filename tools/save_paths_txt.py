4b9f226c7215cda4a7fab9acb8eec7cebf99042c
ddaniorozco <64428779+ddaniorozco@users.noreply.github.com>
2024-02-12 16:38:11 +0200

    Update save_paths_txt.py

--- tools/save_paths_txt.py
+++ tools/save_paths_txt.py
@@ -1,6 +1,5 @@
 import os
 
-
 def save_image_paths(folder_path, output_file):
     with open(output_file, 'w') as f:
         for root, _, files in os.walk(folder_path):
@@ -8,7 +7,8 @@ def save_image_paths(folder_path, output_file):
                 if file.endswith('.jpg') or file.endswith('.png'):
                     file_path = os.path.join(root, file)
                     f.write(file_path + '\n')
-                    
+
+
 folder_path = '/Users/daniorozco/Desktop/litebc_task/capillary_detection/small_test/data'
 output_file = '/Users/daniorozco/Desktop/litebc_task/capillary_detection/small_test/txt_files/train.txt'
 save_image_paths(folder_path, output_file)
